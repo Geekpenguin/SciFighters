fec9ba57d6b02e364c8ba4e852437b7a84536a03
Collin Babb <collinbabbusa@gmail.com>
2014-04-30 20:26:40 -0500

    Commiting some things so I can get some things

---
+++ Assets/enemyholder.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var Player1 : GameObject;
+var Player : Transform;
+
+function Update () {
+
+	//Player1 = GameObject.Find("player-pistol");
+	//Player = Player1.transform;
+    //transform.LookAt(Player);
+
+}
